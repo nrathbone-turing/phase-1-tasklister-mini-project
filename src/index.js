7cdd3251bf4f56ae7a4e46460d023bc3405ad8a6
Nick Rathbone <nrathbone@joinhandshake.com>
2025-06-19 17:20:29 -0600

    Create javascript file that meets deliverables and passes tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    // declare variables for each part of expected usability deliverables
+    const form = document.getElementById("create-task-form");
+    const textInput = document.getElementById("new-task-description");
+    const taskList = document.getElementById("tasks");
+  
+    // type a task into the input field
+
+    form.addEventListener("submit",function(event) {
+        // suppress default action to reload the page after clicking submit
+        event.preventDefault();
+        
+        // create new list item
+        const newTask = document.createElement("li");
+        // set the item's text to be the textInput
+        newTask.textContent = textInput.value;
+        
+        // append newTask to the My Todos list
+        taskList.appendChild(newTask);
+        // clear out form text box after submission
+        form.reset();
+    });
+    
 });
